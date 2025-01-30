2e9ea0195781a8cef6cace9cfd05ccbaf9b901ca
Siqueiraaf <rafael.siqueira098@gmail.com>
2025-01-30 18:52:03 -0300

    Criação do Projeto e Modelo

---
+++ Models/Product.cs
@@ -0,0 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Models
+{
+    namespace backend.Models
+    {
+        public class Product
+        {
+            [Key]
+            public int Id { get; set; } // Identificador primário no banco
+            
+            [Required]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public Guid Uuid { get; set; } = Guid.NewGuid(); // UUID para consultas externas (somente leitura)
+
+            public string Name { get; set; }
+
+            public string Category { get; set; }
+
+            public string AnimalSpecie { get; set; } 
+
+            public string Description { get; set; }
+
+            public decimal Price { get; set; }
+
+            public int StockQuantity { get; set; } 
+            
+            public Product()
+            {
+                Uuid = Guid.NewGuid();
+            }
+        }
+    }    
+}
